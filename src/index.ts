152a8d30b42050ef1a0d74e9c109b80d4118de8d
nazariyv <nazariy@inbox.ru>
2021-07-01 11:55:22 +0100

    export unpackPrice

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { ReNFT } from './renft';
 export { RESOLVER as RESOLVER_ADDRESS, RENFT as RENFT_ADDRESS } from './consts';
 export { Resolver as RESOLVER_ABI, ReNFT as RENFT_ABI } from './abi';
 export { IReNFT, PaymentToken } from './types';
-export { packPrice, toPaddedHex, prepareBatch } from './utils';
+export { packPrice, unpackPrice, toPaddedHex, prepareBatch } from './utils';
