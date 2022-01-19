44b838c45453271dc5504cf5336642e032d82b9a
nazariyv <nazariy@inbox.ru>
2022-01-19 14:18:13 +0000

    adds: new max price and polygon chain id

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
-export const MAX_PRICE = 9999.9999;
+// struct Price { uint24 whole; uint8 decimal; }
+export const MAX_PRICE = 16777215.255;
 export const NUM_BITS_IN_BYTE = 8;
 
 export const NETWORK = {
@@ -7,7 +8,9 @@ export const NETWORK = {
   RINKEBY: 4,
   GOERLI: 5,
   KOVAN: 42,
+  POLYGON: 137
 };
 
-export const RESOLVER = '0x945e589a4715d1915e6fe14f08e4887bc4019341';
-export const RENFT = '0x94d8f036a0fbc216bb532d33bdf6564157af0cd7';
+// todo
+export const RESOLVER = '<>';
+export const RENFT = '<>';
