7832956ff47882ca3e5649a17fcc7870d48e3adf
nazariyv <nazariy@inbox.ru>
2022-05-18 13:49:49 +0100

    feat: support polygon sylvester

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,4 @@ export const NUM_BITS_IN_BYTE = 8;
 export const ResolverAddress = '0x945e589a4715d1915e6fe14f08e4887bc4019341';
 export const AzraelAddress = '0x94d8f036a0fbc216bb532d33bdf6564157af0cd7';
 export const SylvesterAddress = '0xa8D3F65b6E2922fED1430b77aC2b557e1fa8DA4a';
+export const SylvesterPolygonAddress = '0xfA06cFE34C85Ec6b6D29A6a99806cC68BA0018Fe';
