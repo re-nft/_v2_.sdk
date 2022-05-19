cdc4651148904793fb98249faad553c0caf34e66
naz <hi@naz.ooo>
2022-05-19 11:47:11 +0100

    bugfix: added polygon resolver address

--- dist/consts.d.ts
+++ dist/consts.d.ts
@@ -1,6 +1,7 @@
 export declare const MAX_PRICE = 9999.9999;
 export declare const NUM_BITS_IN_BYTE = 8;
 export declare const ResolverAddress = "0x945e589a4715d1915e6fe14f08e4887bc4019341";
+export declare const ResolverPolygonAddress = "0x6884d88Ce56C5C93F46eE23684eBA8628c90B518";
 export declare const AzraelAddress = "0x94d8f036a0fbc216bb532d33bdf6564157af0cd7";
 export declare const SylvesterAddress = "0xa8D3F65b6E2922fED1430b77aC2b557e1fa8DA4a";
 export declare const SylvesterPolygonAddress = "0xfA06cFE34C85Ec6b6D29A6a99806cC68BA0018Fe";
--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,8 @@ export const MAX_PRICE = 9999.9999;
 export const NUM_BITS_IN_BYTE = 8;
 
 export const ResolverAddress = '0x945e589a4715d1915e6fe14f08e4887bc4019341';
+export const ResolverPolygonAddress =
+  '0x6884d88Ce56C5C93F46eE23684eBA8628c90B518';
 export const AzraelAddress = '0x94d8f036a0fbc216bb532d33bdf6564157af0cd7';
 export const SylvesterAddress = '0xa8D3F65b6E2922fED1430b77aC2b557e1fa8DA4a';
 export const SylvesterPolygonAddress =
