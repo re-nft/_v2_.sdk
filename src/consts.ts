82b352406e23e4dfaf79e2e957a1a00fbbdba49f
nazariyv <hi@naz.ooo>
2022-07-06 14:00:58 +0100

    chore: update whoopi fuji addresses

--- src/consts.ts
+++ src/consts.ts
@@ -8,14 +8,14 @@ export const ResolverAddress = '0x945e589a4715d1915e6fe14f08e4887bc4019341';
 export const ResolverPolygonAddress =
   '0x6884d88Ce56C5C93F46eE23684eBA8628c90B518';
 export const ResolverAvalancheAddress =
-  '0xa87EE372743D5A07fc701E5c7b6bCb2204f46607';
+  '0xD41e46d44f55bC93C33D8cDd7E2CB5BE3e93A4c0';
 
 export const AzraelAddress = '0x94d8f036a0fbc216bb532d33bdf6564157af0cd7';
 export const SylvesterAddress = '0xa8D3F65b6E2922fED1430b77aC2b557e1fa8DA4a';
 export const SylvesterPolygonAddress =
   '0xfA06cFE34C85Ec6b6D29A6a99806cC68BA0018Fe';
 export const WhoopiAvalancheAddress =
-  '0xEE9F125748aacCDe3BcD94758395f5AcD41e05a1';
+  '0x516775e81b0d1fC91Ec326DEd21c33728895Fc6C';
 
 // Resolver related
 const SENTINEL: PaymentTokenDetails = {
