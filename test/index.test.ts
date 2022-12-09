eb2ae482ee07d1e36a656407f2eacd1d512e6c2f
cawfree <hello@cawfree.com>
2022-12-09 15:21:33 +0000

    chore: verify correct abi lookup to json modules

---
+++ test/index.test.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import isEqual from 'react-fast-compare';
+
+import {
+  AZRAEL_ABI,
+  RESOLVER_ABI,
+  SYLVESTER_ABI,
+  WHOOPI_ABI,
+} from '../src';
+
+import azrael_v0 from '../src/abi/azrael.v0.abi.json';
+import resolver_v0 from '../src/abi/resolver.v0.abi.json';
+import sylvester_v0 from '../src/abi/sylvester.v0.abi.json';
+import whoopi_v0 from '../src/abi/whoopi.v0.abi.json';
+
+describe('module exports', () => {
+  it ('abis', () => {
+    expect(isEqual(AZRAEL_ABI.abi, azrael_v0)).to.be.true;
+    expect(isEqual(RESOLVER_ABI.abi, resolver_v0)).to.be.true;
+    expect(isEqual(SYLVESTER_ABI.abi, sylvester_v0)).to.be.true;
+    expect(isEqual(WHOOPI_ABI.abi, whoopi_v0)).to.be.true;
+  });
+});
