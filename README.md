5422e6343e41f8ffaa882229ccfd7f35a3842312
cawfree <hello@cawfree.com>
2022-12-12 15:45:49 +0000

    fix: enforce deployment

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ The below is a simple example of lending an ERC721, note that the amount is igno
 ```javascript
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
-import { Azrael, PaymentToken } from '@renft/sdk';
+import { DEPLOYMENT_AZRAEL_ETHEREUM_MAINNET_V0, PaymentToken } from '@renft/sdk';
 
 // const walletMnemonic = Wallet.fromMnemonic(`<your mnemonic>`);
 const provider = new JsonRpcProvider('<your provider uri>');
@@ -37,7 +37,10 @@ let wallet = new Wallet(privKey);
 wallet = wallet.connect(provider);
 
 const main = async () => {
-  const renft = new Azrael(wallet);
+  const renft = getVersionedContractInterfaceForDeployment({
+    deployment: DEPLOYMENT_AZRAEL_ETHEREUM_MAINNET_V0,
+    signer: wallet,
+  });
 
   const nftAddress = ['0xCDf60B46Fa88e74DE7e1e613325E386BFe8609ad'];
   const tokenId = ['3'];
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import isEqual from 'react-fast-compare';
 
 import {
+  AzraelV0FunctionInterface,
   DEPLOYMENT_AZRAEL_ETHEREUM_MAINNET_V0,
   DEPLOYMENT_RESOLVER_AVALANCHE_FUJI_TESTNET_V0,
   DEPLOYMENT_RESOLVER_AVALANCHE_MAINNET_V0,
@@ -12,12 +13,14 @@ import {
   DEPLOYMENT_WHOOPI_AVALANCHE_FUJI_TESTNET_V0,
   DEPLOYMENT_WHOOPI_AVALANCHE_MAINNET_V0,
   getDeploymentAbi,
+  getVersionedContractInterfaceForDeployment,
 } from '../src';
 
 import azrael_v0 from '../src/abi/azrael.v0.abi.json';
 import resolver_v0 from '../src/abi/resolver.v0.abi.json';
 import sylvester_v0 from '../src/abi/sylvester.v0.abi.json';
 import whoopi_v0 from '../src/abi/whoopi.v0.abi.json';
+import {Signer} from "@ethersproject/abstract-signer";
 
 describe('module exports', () => {
   it('abis', () => {
@@ -31,5 +34,16 @@ describe('module exports', () => {
     expect(isEqual(getDeploymentAbi(DEPLOYMENT_RESOLVER_POLYGON_MAINNET_V0), resolver_v0)).to.be.true;
     expect(isEqual(getDeploymentAbi(DEPLOYMENT_RESOLVER_AVALANCHE_FUJI_TESTNET_V0), resolver_v0)).to.be.true;
     expect(isEqual(getDeploymentAbi(DEPLOYMENT_RESOLVER_AVALANCHE_MAINNET_V0), resolver_v0)).to.be.true;
+  });
+  it('readme compatibility', () => {
+    // @ts-expect-error
+    const wallet: Signer = null;
+    const renft = getVersionedContractInterfaceForDeployment({
+      deployment: DEPLOYMENT_AZRAEL_ETHEREUM_MAINNET_V0,
+      signer: wallet,
+    });
+    // HACK: Here we ensure that the returned interface matches the provided deployment.
+    const azrael: AzraelV0FunctionInterface = renft;
+    expect(!!azrael).to.be.true;
   });
 });
