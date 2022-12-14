4803f4b62143a8a95f95b48bdc7ec8a20cebf7e7
cawfree <hello@cawfree.com>
2022-12-14 16:06:17 +0000

    chore: build

--- dist/sdk.cjs.development.js
+++ dist/sdk.cjs.development.js
@@ -4719,7 +4719,8 @@ var RESOLVER_ADDRESS = /*#__PURE__*/getContractAddressForDeployment({
 });
 var RESOLVER_POLYGON_ADDRESS = /*#__PURE__*/getContractAddressForDeployment({
   network: NETWORK_POLYGON_MAINNET,
-  contractType: exports.RenftContractType.RESOLVER
+  contractType: exports.RenftContractType.RESOLVER,
+  version: exports.ResolverVersion.V0
 });
 var RESOLVER_FUJI_ADDRESS = /*#__PURE__*/getContractAddressForDeployment({
   network: NETWORK_AVALANCHE_FUJI_TESTNET,
