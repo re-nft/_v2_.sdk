d834a9f08aa17465811f174af4722568d2a01bd4
cawfree <hello@cawfree.com>
2023-01-17 13:34:08 +0000

    chore: remove dist/

--- dist/contracts/azrael/utils.d.ts
+++
@@ -1,7 +0,0 @@
-import { Contract } from '@ethersproject/contracts';
-import { AzraelV0ClaimCollateralFunction, AzraelV0LendFunction, AzraelV0RentFunction, AzraelV0ReturnItFunction, AzraelV0StopLendingFunction } from './types';
-export declare const createAzraelV0LendThunk: (contract: Contract) => AzraelV0LendFunction;
-export declare const createAzraelV0RentThunk: (contract: Contract) => AzraelV0RentFunction;
-export declare const createAzraelV0ReturnItThunk: (contract: Contract) => AzraelV0ReturnItFunction;
-export declare const createAzraelV0ClaimCollateralThunk: (contract: Contract) => AzraelV0ClaimCollateralFunction;
-export declare const createAzraelV0StopLendingThunk: (contract: Contract) => AzraelV0StopLendingFunction;
