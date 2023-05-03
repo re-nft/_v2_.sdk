3028a9b9490d213ad2be967e10b528821919aed6
nazariyv <hi@naz.ooo>
2023-05-03 22:24:35 +0100

    feat(examples): better examples

--- examples/src/v1.ts
+++ examples/src/v1.ts
@@ -1,24 +1,21 @@
-const { JsonRpcProvider } = require('@ethersproject/providers');
-const { Wallet } = require('@ethersproject/wallet');
-const { BigNumber } = require('@ethersproject/bignumber');
-const {
+import { JsonRpcProvider } from "@ethersproject/providers";
+import { Wallet } from "@ethersproject/wallet";
+import {
   DEPLOYMENT_SYLVESTER_ETHEREUM_MAINNET_V0,
-  PaymentToken,
   getRenftContract,
-} = require('@renft/sdk');
+  PaymentToken,
+} from "@renft/sdk";
 
 // const walletMnemonic = Wallet.fromMnemonic(`<your mnemonic>`);
-const provider = new JsonRpcProvider('<your provider uri>');
-const privKey = '<privateKey>';
+const provider = new JsonRpcProvider("<your provider uri>");
+const privKey = "<privateKey>";
 let wallet = new Wallet(privKey);
 wallet = wallet.connect(provider);
 
 let txn;
 let receipt;
 
-
 const main = async () => {
-
   // ---------------- LENDING ----------------------
 
   // collateral solution
@@ -29,8 +26,8 @@ const main = async () => {
     signer: wallet,
   });
 
-  const E721_ADDR = ['0xCDf60B46Fa88e74DE7e1e613325E386BFe8609ad'];
-  const E721_TOKENID = [BigNumber.from('3')];
+  const E721_ADDR = ["0xCDf60B46Fa88e74DE7e1e613325E386BFe8609ad"];
+  const E721_TOKENID = ["3"];
   const lendAmount = [1];
   const maxRentDuration = [1];
   const dailyRentPrice = [1.1];
@@ -38,21 +35,28 @@ const main = async () => {
   const paymentToken = [PaymentToken.WETH];
 
   const nftStandard = [1, 1];
-  const nftAddress = ['0x495f947276749ce646f68ac8c248420045cb7b5e', '0xd07dc4262bcdbf85190c01c996b4c06a461d2430'];
-  const tokenID = ['65385238396461245548909757441444140644847314737463192205847680215270719225857', '73166'];
-  const lendingID = [133, 132];
+  const nftAddress = [
+    "0x495f947276749ce646f68ac8c248420045cb7b5e",
+    "0xd07dc4262bcdbf85190c01c996b4c06a461d2430",
+  ];
+  const tokenID = [
+    "65385238396461245548909757441444140644847314737463192205847680215270719225857",
+    "73166",
+  ];
+  const lendingID = ['133', '132'];
   const rentDuration = [1, 1];
-  const rentAmount = [1, 1];
+  const rentAmount = ['1', '1'];
 
   // Azrael Lend Transaction
   txn = await renft.lend(
+    nftStandard,
     E721_ADDR,
     E721_TOKENID,
     lendAmount,
     maxRentDuration,
     dailyRentPrice,
     nftPrice,
-    paymentToken
+    paymentToken,
   );
   receipt = await txn.wait();
 
@@ -65,7 +69,7 @@ const main = async () => {
     tokenID,
     lendingID,
     rentDuration,
-    rentAmount
+    rentAmount,
   );
   receipt = await txn.wait();
 
@@ -73,9 +77,9 @@ const main = async () => {
 };
 
 main()
-  .then(receipt => {
+  .then((receipt) => {
     console.log(receipt);
   })
-  .catch(e => {
+  .catch((e) => {
     console.warn(e);
   });
