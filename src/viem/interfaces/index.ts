0e7380ded9ec4c1f43a96d8dc22ab3a26011186f
Rommert Zijlstra <rommert.thomas@gmail.com>
2023-06-08 21:03:47 +0200

    "Azreal" -> "Azrael"

--- src/viem/interfaces/index.ts
+++ src/viem/interfaces/index.ts
@@ -1,6 +1,6 @@
-import { default as AzrealV0SDK } from './Azrael.v0';
+import { default as AzraelV0SDK } from './Azrael.v0';
 import { default as SylvesterV0SDK } from './Sylvester.v0';
 import { default as SylvesterV1SDK } from './Sylvester.v1';
 import { default as WhoopiV0SDK } from './Whoopi.v0';
 
-export { AzrealV0SDK, SylvesterV0SDK, SylvesterV1SDK, WhoopiV0SDK };
+export { AzraelV0SDK, SylvesterV0SDK, SylvesterV1SDK, WhoopiV0SDK };
